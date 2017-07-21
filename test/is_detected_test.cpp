fd896674aaed76e6430faf2d2d983689d3f3e07e
Jake Askeland <jake.askeland@gmail.com>
2017-07-20 22:21:26 -0700

    improved is_detected tests

--- test/is_detected_test.cpp
+++ test/is_detected_test.cpp
@@ -2,6 +2,13 @@
 
 using namespace jakalib;
 
+/**
+ * Test copy assignment detection.
+ *
+ * Origin:
+ *   http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2015/n4502.pdf
+ */
+
 template <typename T>
 using copy_assign_op = decltype(std::declval<T&>() = std::declval<const T&>());
 
@@ -28,16 +35,23 @@ static_assert(!copy_assignable<bar>::value, "bar is copy assignable");
 static_assert(!canonically_copy_assignable<car>::value,
               "car is canonically copy assignable");
 
-// Inspired by:
-// https://github.com/fuchsia-mirror/third_party-libcxx/blob/master/test/std/
-//   experimental/utilities/meta/meta.detect/is_detected_convertible.pass.cpp
+/**
+ * Test member detection.
+ */
 
 template <typename T>
-using call_foo = decltype(std::declval<T&>().foo());
+using foo_member_op = decltype(std::declval<T&>().foo());
 
 template <typename T>
-using has_int_compatible_foo = is_detected_convertible<int, call_foo, T>;
+using has_foo = is_detected<foo_member_op, T>;
 
+template <typename T>
+using has_int_foo = is_detected_exact<int, foo_member_op, T>;
+
+template <typename T>
+using has_int_compatible_foo = is_detected_convertible<int, foo_member_op, T>;
+
+// Example usage.
 template <typename T>
 int invokeFoo(T t) {
   static_assert(has_int_compatible_foo<T>::value,
@@ -45,29 +59,76 @@ int invokeFoo(T t) {
   return t.foo();
 }
 
-struct an_int {
+struct no_foo {};
+
+struct int_foo {
   int foo() {
     return 1;
   }
 };
 
-struct a_long {
+struct long_foo {
   long foo() {
     return 1l;
   }
 };
 
-struct a_nullptr {
+struct pointer_foo {
   void* foo() {
     return nullptr;
   }
 };
 
-static_assert(!has_int_compatible_foo<a_nullptr>::value,
-              "void* should not be convertible to int.");
+struct int_foo_with_arg {
+  int foo(int x) {
+    return x;
+  }
+};
 
-int x = invokeFoo(an_int());
-int y = invokeFoo(a_long());
+//
+// is_detected test
+//
+
+template <bool expected, typename TypeUnderTest>
+constexpr bool expectedHasFoo() {
+  return has_foo<TypeUnderTest>::value == expected;
+}
+
+static_assert(expectedHasFoo<false, no_foo>(), "");
+static_assert(expectedHasFoo<true, int_foo>(), "");
+static_assert(expectedHasFoo<true, long_foo>(), "");
+static_assert(expectedHasFoo<true, pointer_foo>(), "");
+static_assert(expectedHasFoo<false, int_foo_with_arg>(), "");
+
+//
+// is_detected_exact test
+//
+
+template <bool expected, typename TypeUnderTest>
+constexpr bool expectedHasIntFoo() {
+  return has_int_foo<TypeUnderTest>::value == expected;
+}
+
+static_assert(expectedHasIntFoo<false, no_foo>(), "");
+static_assert(expectedHasIntFoo<true, int_foo>(), "");
+static_assert(expectedHasIntFoo<false, long_foo>(), "");
+static_assert(expectedHasIntFoo<false, pointer_foo>(), "");
+static_assert(expectedHasIntFoo<false, int_foo_with_arg>(), "");
+
+//
+// is_detected_convertible test
+//
+
+template <bool expected, typename TypeUnderTest>
+constexpr bool expectedHasIntCompatFoo() {
+  return has_int_compatible_foo<TypeUnderTest>::value == expected;
+}
+
+static_assert(expectedHasIntCompatFoo<false, no_foo>(), "");
+static_assert(expectedHasIntCompatFoo<true, int_foo>(), "");
+static_assert(expectedHasIntCompatFoo<true, long_foo>(), "");
+static_assert(expectedHasIntCompatFoo<false, pointer_foo>(), "");
+static_assert(expectedHasIntCompatFoo<false, int_foo_with_arg>(), "");
 
 int main() {
   return 0;
