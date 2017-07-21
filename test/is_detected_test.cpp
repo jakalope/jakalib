da74633aa48157d381a100740ac5a0c3157d66cc
Jake Askeland <jake.askeland@gmail.com>
2017-07-20 21:16:06 -0700

    add tests for is_detected_convertible

--- test/is_detected_test.cpp
+++ test/is_detected_test.cpp
@@ -11,13 +11,6 @@ using copy_assignable = is_detected<copy_assign_op, T>;
 template <typename T>
 using canonically_copy_assignable = is_detected_exact<T&, copy_assign_op, T>;
 
-// template <typename T, typename... Args>
-// using binary_op =
-// decltype(std::declval<T>.operator()(std::declval<Args>()...));
-
-// template <typename T>
-// using is_binary_op = is_detected_convertible<bool, binary_op, T>;
-
 struct foo {};
 
 struct bar {
@@ -28,20 +21,54 @@ struct car {
   void operator=(const car&) {}
 };
 
-struct BinaryFunctor {
-  bool operator()() {
-    return true;
+static_assert(copy_assignable<foo>::value, "foo is not copy assignable");
+
+static_assert(!copy_assignable<bar>::value, "bar is copy assignable");
+
+static_assert(!canonically_copy_assignable<car>::value,
+              "car is canonically copy assignable");
+
+// Inspired by:
+// https://github.com/fuchsia-mirror/third_party-libcxx/blob/master/test/std/
+//   experimental/utilities/meta/meta.detect/is_detected_convertible.pass.cpp
+
+template <typename T>
+using call_foo = decltype(std::declval<T&>().foo());
+
+template <typename T>
+using has_int_compatible_foo = is_detected_convertible<int, call_foo, T>;
+
+template <typename T>
+int invokeFoo(T t) {
+  static_assert(has_int_compatible_foo<T>::value,
+                "T must have an int-compatible member foo()");
+  return t.foo();
+}
+
+struct an_int {
+  int foo() {
+    return 1;
   }
 };
 
+struct a_long {
+  long foo() {
+    return 1l;
+  }
+};
+
+struct a_nullptr {
+  void* foo() {
+    return nullptr;
+  }
+};
+
+static_assert(!has_int_compatible_foo<a_nullptr>::value,
+              "void* should not be convertible to int.");
+
+int x = invokeFoo(an_int());
+int y = invokeFoo(a_long());
+
 int main() {
-  static_assert(copy_assignable<foo>::value, "foo is not copy assignable");
-
-  static_assert(!copy_assignable<bar>::value, "bar is copy assignable");
-
-  static_assert(!canonically_copy_assignable<car>::value,
-                "car is canonically copy assignable");
-  // static_assert(is_binary_op<BinaryFunctor>::value,
-  //               "BinaryFunctor isn't a binary operator");
   return 0;
 }
