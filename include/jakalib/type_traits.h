190b7bcee6578a7a70fd1e57c948dc1a858df5dc
Ryan Cahoon <ryan.cahoon@zoox.com>
2017-10-03 16:20:18 -0700

    Add implementaton of std::size, added in C++17

--- include/jakalib/type_traits.h
+++ include/jakalib/type_traits.h
@@ -6,6 +6,20 @@
 namespace jakalib {
 
 //
+// Container accessors
+//
+
+template <class C>
+constexpr auto size(const C& c) -> decltype(c.size()) {
+  return c.size();
+}
+
+template <class T, std::size_t N>
+constexpr std::size_t size(const T(&)[N]) noexcept {
+  return N;
+}
+
+//
 // bool_constant
 //
 
